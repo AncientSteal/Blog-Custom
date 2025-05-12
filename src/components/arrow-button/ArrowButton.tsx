c642510b06080f3964bea9cbc72185c12a533578
AncientSteal <bezlepkinartur@gmail.com>
2025-05-12 18:07:30 +0700

    Добавлена основная функциональность блога

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,21 +5,26 @@ import arrow from 'src/images/arrow.svg';
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type ArrowButtonProps = {
+	isOpen: boolean; 
+	onClick: () => void;
+};
 
-export const ArrowButton = () => {
+export const ArrowButton = (props: ArrowButtonProps) => {
+	const { onClick, isOpen } = props;
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}
+			className={clsx(styles.container, isOpen && styles.container_open)}
+			onClick={() => onClick()}
 		>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow}
+				className={clsx(styles.arrow, isOpen && styles.arrow_open)}
 			/>
 		</div>
 	);
